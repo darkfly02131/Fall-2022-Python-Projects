81cabb212562e4cc0682cc0da7e60c3643b8ddd1
Zachary Lord <lordz@go.stockton.edu>
2024-01-03 18:59:44 -0500

    Update Project_12_1_Zachary_Lord.py

--- Project_12_1_Zachary_Lord.py
+++ Project_12_1_Zachary_Lord.py
@@ -1,8 +1,10 @@
 #Zachary Lord
 from Rectangle import Rectangle
 def main():
+    #Welcome menu
     print('Rectangle Calculator')
     while True:
+        #Calculations and formattiing
         height = input('{:11}'.format('Height:'))
         width = input('{:11}'.format('Width:'))
         p1 = Rectangle(height, width)
@@ -10,7 +12,7 @@ def main():
         print("{:10}".format('Area:'),p1.area())
         p1.createRectangle()
         print()
-    
+        #Choice to see if the user wants to continue or not.
         choice = input('Continue? (y/n):')
         if choice == 'n':
             break
