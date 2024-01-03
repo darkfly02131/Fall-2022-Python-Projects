0d72a707c01b9d002588548a60648a1d115c8a49
Zachary Lord <lordz@go.stockton.edu>
2024-01-03 18:57:51 -0500

    Update Project_11_1_Zachary Lord.py

--- Project_11_1_Zachary Lord.py
+++ Project_11_1_Zachary Lord.py
@@ -1,7 +1,10 @@
+#Welcome menu
 print('HTML Converter')
 print()
+#Opens file and reads lines
 myfile = open('p9-1_groceries.html')
 lines = myfile.readlines()
+#replaces all the lines
 for line in lines:
     lines= line.replace('h1', ' ').strip().replace('<', ' ').replace('>', ' ').replace('/', ' ').replace('ul', ' ').replace('li', '').strip().replace('Eggs', '* Eggs ').strip().replace('Milk', '* Milk ').replace('Butter', '* Butter ')
     print(lines)
