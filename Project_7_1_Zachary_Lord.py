0fa857293d68a230630a8d97b6c07d5d7dafc103
Zachary Lord <lordz@go.stockton.edu>
2024-01-03 19:02:49 -0500

    Update Project_7_1_Zachary_Lord.py

--- Project_7_1_Zachary_Lord.py
+++ Project_7_1_Zachary_Lord.py
@@ -1,5 +1,6 @@
 #Zachary Lord
-
+#Duplicate program:
+#Shows the display menu for the program
 def display_menu():
     print('The Wizard Inventory program\n')
     print('COMMAND MENU')
@@ -9,34 +10,34 @@ def display_menu():
     print('drop - Drop an Item')
     print('exit - Exit an item')
     print()
-
+#shows the items in the inventory
 def show_item(inventory):
     i= 1
     for item in inventory:
         print(str(i) +'.' + item)
         i+=1
     print()
-
+#grabs the items in the inventory
 def grab_item(inventory):
     item= input('Name:')
     inventory.append(item)
     write_inventory(inventory)
     print(item + ' was added.')
         
-
+#edits the items in the inventory
 def edit_item(inventory):
     num = int(input('Number: '))
     name = input('Name: ')
     inventory[num-1]= name
     write_inventory(inventory)
     print(inventory[num-1],'was updated.')
-
+#drops/deletes an item in the inventory
 def drop_item(inventory):
     num= int(input('Number: '))
     inv = inventory.pop(num-1)
     write_inventory(inventory)
     print(inv +'  was deleted.\n')
-
+#reads the items from the wizard_all_items.txt file.
 def read_inventory():
     inventory = []
     with open('wizard_all_items.txt') as file:
@@ -44,6 +45,7 @@ def read_inventory():
             line = line.replace("\n", "")
             inventory.append(line)
     return inventory
+#writes the items from the file.
 def write_inventory(inventory):
     with open('wizard_all_items.txt', "w") as file:
         for inv in inventory:
@@ -54,6 +56,7 @@ def main():
     display_menu()
 
 
+   #Functionality for the display menu
     while True:
         command= input('Command: ')
         if command  == 'show':
