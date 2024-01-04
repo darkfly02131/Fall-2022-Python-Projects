105d3bb5c799e69d639f51839d3c80767f4b9c87
Zachary Lord <lordz@go.stockton.edu>
2024-01-03 19:05:25 -0500

    Update Rectangle.py

--- Rectangle.py
+++ Rectangle.py
@@ -1,12 +1,17 @@
 #Zachary Lord
+#The rectangle module for Project_12_1
+#intializes Class
 class Rectangle:
     def __init__(self, height, width):
         self.height = int(height)
         self.width = int(width)
+    #creates area
     def area(self):
         return self.height * self.width
+    #creates perimeter
     def perimeter(self):
         return (2*(self.height+self.width))
+    #Creates rectangle based on L, W, & H.
     def createRectangle(self):
         firstLine= "* " * self.width
         middleLine = '*' + '  '* (self.width - 2) + " *"
